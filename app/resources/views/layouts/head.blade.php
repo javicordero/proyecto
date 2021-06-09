8bda87d336f24bf6c52c30079b99236fd64336d1
Javier Cordero Toscano <javiercorderotoscano@gmail.com>
2021-06-09 21:13:59 +0200

    Plantilla publica

---
+++ app/resources/views/layouts/head.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Phoenix - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900&display=swap" >
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" >
+
+    <link rel="stylesheet" href="{{ asset('public-template/css/open-iconic-bootstrap.min.css') }}" >
+    <link rel="stylesheet" href="{{ asset('public-template/css/animate.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('public-template/css/owl.carousel.min.css') }}" >
+    <link rel="stylesheet" href="{{ asset('public-template/css/owl.theme.default.min.css') }}" >
+    <link rel="stylesheet" href="{{ asset('public-template/css/magnific-popup.css') }}" >
+
+    <link rel="stylesheet" href="{{ asset('public-template/css/aos.css') }}" >
+
+    <link rel="stylesheet" href="{{ asset('public-template/css/ionicons.min.css') }}" >
+
+    <link rel="stylesheet" href="{{ asset('public-template/css/bootstrap-datepicker.css') }}" >
+    <link rel="stylesheet" href="{{ asset('public-template/css/jquery.timepicker.css') }}" >
+
+
+    <link rel="stylesheet" href="{{ asset('public-template/css/flaticon.css') }}" >
+    <link rel="stylesheet" href="{{ asset('public-template/css/icomoon.css') }}" >
+    <link rel="stylesheet" href="{{ asset('public-template/css/style.css') }}" >
+
+
+  </head>
---
+++ app/resources/views/layouts/jsIncludes.blade.php
@@ -0,0 +1,26 @@
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+ <script src="{{ asset('public-template/js/jquery.min.js') }}"></script>
+ <script src="{{ asset('public-template/js/jquery-migrate-3.0.1.min.js') }}"></script>
+ <script src="{{ asset('public-template/js/popper.min.js') }}"></script>
+ <script src="{{ asset('public-template/js/bootstrap.min.js') }}"></script>
+ <script src="{{ asset('public-template/js/jquery.easing.1.3.js') }}"></script>
+ <script src="{{ asset('public-template/js/jquery.waypoints.min.js') }}"></script>
+ <script src="{{ asset('public-template/js/jquery.stellar.min.js') }}"></script>
+ <script src="{{ asset('public-template/js/owl.carousel.min.js') }}"></script>
+ <script src="{{ asset('public-template/js/jquery.magnific-popup.min.js') }}"></script>
+ <script src="{{ asset('public-template/js/aos.js') }}"></script>
+ <script src="{{ asset('public-template/js/jquery.animateNumber.min.js') }}"></script>
+ <script src="{{ asset('public-template/js/bootstrap-datepicker.js') }}"></script>
+ <script src="{{ asset('public-template/js/scrollax.min.js') }}"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false') }}"></script>
+ <script src="{{ asset('public-template/js/google-map.js') }}"></script>
+ <script src="{{ asset('public-template/js/main.js') }}"></script>
+ <script src="https://kit.fontawesome.com/4ef78eb583.js" crossorigin="anonymous"></script>
+
+  </body>
+</html>
+
